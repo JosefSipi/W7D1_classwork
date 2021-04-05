c56e18d41f31cae4ad0d578dd9b0e90501a38469
Joseph <Josephsipiorski@gmail.com>
2021-04-05 16:41:57 -0500

    up to cats have an owner

--- ninety_nine_cats_auth/app/models/user.rb
+++ ninety_nine_cats_auth/app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
 
+  has_many :cats,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :Cat
+
   def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
     self.save!
--- ninety_nine_cats_auth/db/schema.rb
+++ ninety_nine_cats_auth/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_181147) do
+ActiveRecord::Schema.define(version: 2021_04_05_212111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2021_04_05_181147) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_cats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_04_05_181147) do
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
+  add_foreign_key "cats", "users"
 end
