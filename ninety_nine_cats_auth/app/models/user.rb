5109ab54eb22586c7722ba7f061ed4bded844e1f
fdiengott <freddiengott@gmail.com>
2021-04-05 14:33:19 -0400

    Add users table, model, user class methods

---
+++ ninety_nine_cats_auth/app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  validates :session_token, :user_name, uniqueness: true, presence: true
+  # validates :password, length: { minimum: 6, allow_nil: true }
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+    # why not save!?
+  end
+
+  def password
+    @password
+  end
+
+  def is_password?(password)
+    pw_obj = BCrypt::Password.new(password_digest)
+    pw_obj.is_password?(password)
+  end
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+
+    if user && user.is_password?(password)
+      user
+    else 
+      nil
+    end
+  end
+
+
+end
